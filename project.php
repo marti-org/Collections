b1fdbcc1bbdd68fc167c4faa95c658de9a7be3b3
marti-org <martinorava@seznam.cz>
2015-02-13 23:00:52 +0200

    Test smartgitu

--- project.php
+++ project.php
@@ -42,8 +42,69 @@
     }
   </style>
 
+  <style>
+  #draggable { width: 150px; height: 150px; padding: 0.5em; }
+  </style>
+  <script>
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
+  </script>
+
+  <style>
+  .toggler { width: 500px; height: 200px; }
+  #button { padding: .5em 1em; text-decoration: none; }
+  #effect { width: 240px; height: 135px; padding: 0.4em; position: relative; }
+  #effect h3 { margin: 0; padding: 0.4em; text-align: center; }
+  </style>
+  <script>
+  $(function() {
+    // run the currently selected effect
+    function runEffect() {
+      // get effect type from
+      var selectedEffect = $( "#effectTypes" ).val();
+ 
+      // most effect types need no options passed by default
+      var options = {};
+      // some effects have required parameters
+      if ( selectedEffect === "scale" ) {
+        options = { percent: 100 };
+      } else if ( selectedEffect === "size" ) {
+        options = { to: { width: 280, height: 185 } };
+      }
+ 
+      // run the effect
+      $( "#effect" ).show( selectedEffect, options, 500, callback );
+    };
+ 
+    //callback function to bring a hidden box back
+    function callback() {
+      setTimeout(function() {
+        $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+      }, 1000 );
+    };
+ 
+    // set effect from select menu value
+    $( "#button" ).click(function() {
+      runEffect();
+    });
+ 
+    $( "#effect" ).hide();
+  });
+  </script>
+
+
   </head>
   <body>
+
+    <div class="toggler">
+          <div id="effect" class="ui-widget-content ui-corner-all">
+            <h3 class="ui-widget-header ui-corner-all">Show</h3>
+            <p>
+              Etiam libero neque, luctus a, eleifend nec, semper at, lorem. Sed pede. Nulla lorem metus, adipiscing ut, luctus sed, hendrerit vitae, mi.
+            </p>
+          </div>
+    </div>
 
     <div id="page">
        <div id="top">
@@ -96,8 +157,14 @@
             </table>
             
 
+        </div class="ui-widget-content">
+        <h2> Chapter 1 </h2>
+        <div id="draggable" >
+            <img src="edit.png">
+            <textarea rows="4" cols="70"> </textarea> </td>
         </div>
-        <div> Floating div </div>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/9XFeW9dgyIs?list=UUAuUUnT6oDeKwE6v1NGQxug" frameborder="0" allowfullscreen></iframe>
     </div>
     
 
